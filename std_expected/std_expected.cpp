36e441ed5306ae03da00cb9104b6ca4b47e8e127
Muhammad Awais Khalid <awais.khalid.awan@gmail.com>
2025-01-02 22:06:19 +0500

    Updated std_expected.cpp

--- std_expected/std_expected.cpp
+++ std_expected/std_expected.cpp
@@ -17,12 +17,12 @@ int main()
 {
     std::cout << "Demonstration of std::expected in C++" << std::endl << std::endl;
 
-    auto result = performDivision(10, 1);       // Call the function to perfrom the operation.
+    auto result = performDivision(10, 1);       // Call the function to perform the operation.
 
     if (result.has_value()) {                   // Check if the result has a valid value. Print the value.
         std::cout << "Result: " << result.value() << std::endl;
     }
-    else {                      // If there are no valid values in the result than it contains and unexpected error value.
+    else {                      // If there are no valid values in the result than it contains an unexpected error value.
         std::cout << "An error occured while calling performDivision(): " << result.error() << std::endl;
     }
 
